2c36b765cd3810914eb9b7bf32d84378b9ab2297
shaojung <shaojung@gmail.com>
2016-08-04 14:12:27 +0800

    讀出內部檔案

--- app/src/main/java/tw/com/pcschool/t080402/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080402/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
@@ -19,13 +20,29 @@ public class MainActivity extends AppCompatActivity {
 
         File f = getCacheDir();
         Log.d("FILE", f.toString());
-        String fName = "mydata";
+        String fName = "mydata.txt";
         FileOutputStream fOut = null;
         try {
+
             fOut = openFileOutput(fName, MODE_PRIVATE);
             OutputStreamWriter osw = new OutputStreamWriter(fOut);
             osw.write("She sell sea shells on the sea shore .");
             osw.close();
+
+
+            File readFile = new File(getFilesDir() + File.separator + fName);
+            char[] buffer = new char[1];
+            FileReader fr = null;
+            StringBuilder sb = new StringBuilder();
+
+            fr = new FileReader(readFile);
+            while (fr.read(buffer)!= -1) {
+                sb.append(new String(buffer));
+            }
+            fr.close();
+            Log.d("FILE", sb.toString());
+
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
