45bb20947f0cbbe714ffa373bd00251ba36e535b
shaojung <shaojung@gmail.com>
2016-08-04 13:55:17 +0800

    寫入內部檔案

--- app/src/main/java/tw/com/pcschool/t080402/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080402/MainActivity.java
@@ -2,6 +2,13 @@ package tw.com.pcschool.t080402;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        File f = getCacheDir();
+        Log.d("FILE", f.toString());
+        String fName = "mydata";
+        FileOutputStream fOut = null;
+        try {
+            fOut = openFileOutput(fName, MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            osw.write("She sell sea shells on the sea shore .");
+            osw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
