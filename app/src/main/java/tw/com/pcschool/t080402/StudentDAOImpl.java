c775c4900537cdbe43319517c7bc44c583f942c7
shaojung <shaojung@gmail.com>
2016-08-05 10:00:03 +0800

    完成 DAO 第一步。

--- app/src/main/java/tw/com/pcschool/t080402/StudentDAOImpl.java
+++ app/src/main/java/tw/com/pcschool/t080402/StudentDAOImpl.java
@@ -1,5 +1,16 @@
 package tw.com.pcschool.t080402;
 
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +19,40 @@ import java.util.List;
  */
 public class StudentDAOImpl implements StudentDAO
 {
-    ArrayList<Student> mylist;
+    static ArrayList<Student> mylist;
+    Context context;
+    public StudentDAOImpl(Context context)
+    {
+        this.context = context;
+        String fName = "student.json";
+        File readFile = new File(context.getFilesDir() + File.separator + fName);
+        char[] buffer = new char[1];
+        FileReader fr = null;
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            fr = new FileReader(readFile);
+            while (fr.read(buffer)!= -1) {
+                sb.append(new String(buffer));
+            }
+            fr.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String data = sb.toString();
+        if (data.length() == 0)
+        {
+            mylist = new ArrayList<>();
+        }
+        else
+        {
+            Gson gson = new Gson();
+            mylist = gson.fromJson(data, new TypeToken<ArrayList<Student>>() {}.getType());
+        }
+    }
+
     @Override
     public void addStudent(Student s) {
 
